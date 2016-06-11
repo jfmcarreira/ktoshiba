dcbf84b451cdb1fada807b42305a7c3227e87040
coproscefalo <coproscefalo@9886a1c5-4d1a-0410-95ff-8ba4ff0d3d82>
2016-06-11 19:05:21 +0000

    Update FindLibMNL.cmake as it was causing trouble on some distros
    git-svn-id: https://svn.code.sf.net/p/ktoshiba/code/trunk/ktoshiba@267 9886a1c5-4d1a-0410-95ff-8ba4ff0d3d82

--- cmake/FindLibMNL.cmake
+++ cmake/FindLibMNL.cmake
@@ -8,9 +8,4 @@ find_package(PkgConfig REQUIRED)
 
 pkg_check_modules(LIBMNL REQUIRED libmnl)
 
-find_package_handle_standard_args(LIBMNL DEFAULT_MSG
-	LIBMNL_LIBRARIES
-	LIBMNL_INCLUDE_DIRS
-)
-
 mark_as_advanced(LIBMNL_INCLUDE_DIRS LIBMNL_LIBRARIES)
